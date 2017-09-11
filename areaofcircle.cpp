2951a80b6b460479f04e8014fb474f803db3bd41
RahulDhiman93 <31551130+RahulDhiman93@users.noreply.github.com>
2017-09-11 21:47:23 +0530

    Finding Area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+//print area and cirumference of circle
+#include<iostream>
+using namespace std;
+main()
+{
+int r,a,c;
+cout<<"enter the value of r";
+cin>>r;
+a=(3.14*r*r);
+cout<<"area is"<<"\t"<<a<<"\n";
+c=(3.14*2*r);
+cout<<"cir is"<<c;}
