f9f65ca7d2282de64cfacaa468b2706425ab86b3
RahulDhiman93 <31551130+RahulDhiman93@users.noreply.github.com>
2017-09-11 21:48:56 +0530

    area of rectangle

---
+++ areaofrectangle.cpp
@@ -0,0 +1,14 @@
+//print area of rectangle and square
+#include<iostream>
+using namespace std;
+main()
+{
+int l,b,a,r,s;
+cout<<"vale of l and b and a is";
+cin>>l>>b>>a;
+r=l*b;
+cout<<"area is"<<r;
+s=a*a;
+cout<<"\n"<<"area is"<<s;
+
+}
